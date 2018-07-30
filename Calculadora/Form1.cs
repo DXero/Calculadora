59fa8795d3246853bb073474cbe223d4df9fdcc0
Denny Alexis Herrera Marín <denny_herrera@live.com>
2018-07-29 19:44:39 -0600

    Texto

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -34,7 +34,7 @@ namespace Calculadora
             }
             else if (cbOperacion.SelectedIndex == 3)
             {
-                lblResultado.Text = "Resultado: div " + (double.Parse(txtN1.Text) / double.Parse(txtN2.Text));
+                lblResultado.Text = "Resultado: divi " + (double.Parse(txtN1.Text) / double.Parse(txtN2.Text));
             }
         }
     }
